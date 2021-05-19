485398eab77f92b841c6e483d0f55ca36b5fbe0d
Sinh Vu <sinhvusinh1997@gmail.comm>
2021-05-19 12:10:31 +0700

    test

--- generate-data.js
+++ generate-data.js
@@ -8,7 +8,7 @@ const randomStudentList = n => {
 
   const studentList = [];
 
-  const getBirthDay = faker.date.toDateString();
+  const getBirthDay = faker.date.betweens('1995-01-01', '2000-01-05').toDateString()
 
   Array.from(new Array(n)).forEach(() => {
     let gender = faker.datatype.boolean() ? 'Female' : 'Male';
